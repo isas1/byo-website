b5411af97fd32c31c436c085f64b3e789a0636f9
isas1 <teachmecomputerscience@gmail.com>
2018-10-08 17:07:17 +0100

    Left transition added

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -19,12 +19,20 @@ const imgs = document.querySelectorAll('img');
 window.addEventListener("scroll", debounce(checkSlide));
 
 function checkSlide() {
-
-
   // forEach img in imgs
+  imgs.forEach(function(img) {
+  
   // find the middle of img
-  // find the end of the img
-  // addClass active between middle and end
-  // removeClass active when reaching the end of the img
-  console.log("imgs");
+  const slideIn = (window.scrollY + window.innerHeight) - img.height / 2 // start of transition
+
+  const imgBottom = img.offsetTop + img.height; // bottom of the image is the end of the transition
+  const isHalfShown = slideIn > img.offsetTop; // between middle of image and bottom - to add "active" class
+  const isNotPast = imgBottom > window.scrollY; // above bottom of image - to add "active" class
+
+  if (isHalfShown && isNotPast) { // add 'active' class when slidIn
+        img.classList.add("active");
+      } else { // finish 'active' class when imgBottom
+        img.classList.remove("active"); 
+      }
+  });
 }
